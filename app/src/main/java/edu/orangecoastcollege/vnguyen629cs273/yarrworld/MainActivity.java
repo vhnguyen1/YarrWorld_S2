5b4ce713414d333163e4498f0bad27e9653edfc5
vnguyen629 <brotherflab@gmail.com>
2016-10-09 23:33:11 -0700

    Finished Project

--- app/src/main/java/edu/orangecoastcollege/vnguyen629cs273/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/vnguyen629cs273/yarrworld/MainActivity.java
@@ -1,10 +1,15 @@
 package edu.orangecoastcollege.vnguyen629cs273.yarrworld;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context = this;
 
     // Define the button as an instance variable (member variable)
     Button speakButton;
@@ -20,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Hook up the speakButton to the one in our view:
         speakButton = (Button) findViewById(R.id.speakButton);
+    }
+
+    public void shimberText(View view) {
+        Toast.makeText(context, R.string.toast_text, Toast.LENGTH_LONG).show();
     }
 }
